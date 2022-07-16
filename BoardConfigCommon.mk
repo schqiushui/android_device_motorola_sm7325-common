37a2b791450bbbe49cc4ac03095a86e55c7d95b8
LuK1337 <priv.luk@gmail.com>
2022-07-16 22:24:16 +0200

    sm8350-common: Register IHighTouchPollingRate interface
    Change-Id: I3c48324cd201e810f04ccaa39898d7c8e43fa2e5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -131,6 +131,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     vendor/aosp/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
+ODM_MANIFEST_FILES += $(COMMON_PATH)/manifest_odm.xml
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
