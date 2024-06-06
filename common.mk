895091b14377bbf632d1b33d82c0b6cb7246c097
schqiushui <orochi9999@gmail.com>
2024-06-06 23:58:17 +0800

    Revert "sm8350-common: Build some missing LDAC libs"
    This reverts commit d1d00021ad688d103910fd929ec335df885da771.

--- common.mk
+++ common.mk
@@ -93,9 +93,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor \
-    libldacBT_abr \
-    libldacBT_bco \
-    libldacBT_enc
+    libldacBT_bco
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
